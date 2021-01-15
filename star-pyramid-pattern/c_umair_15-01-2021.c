8fea08c2c5c1cdf5af430b2a48bf4d11573e58f4
Mohammad Umair <38830116+mohammad-umair-GrinRex@users.noreply.github.com>
2021-01-15 17:10:16 +0530

    Upload
    Updated with demo

---
+++ star-pyramid-pattern/c_umair_15-01-2021.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+
+    printf("Enter number of rows: ");
+    scanf("%d",&n);
+
+    for(int i=1; i<=n; i++)
+    {
+      for(int j=1; j <= n-i; j++)
+      printf(" "); // space for
+
+      for(int k=1; k <= (2*i-1); k++)
+      printf("*"); // star for
+
+      printf("\n"); // new line for 
+    }
+
+    return 0;
+}
